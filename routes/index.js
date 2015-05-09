367f5622a2ddbef6c90e6864488d394817475762
Manuel Roth <manuelroth@hotmail.ch>
2015-05-09 11:33:34 +0200

    Added check for session and redirect to login.html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/links');
+  if(req.session.email) {
+    res.redirect('/links');
+  } else {
+    res.sendfile('public/login.html');
+  }
 });
 
 module.exports = router;
